d24c45c498bd100d070b18ff7b3f33e56faac33f
Unknown <derek.pereira@stonybrook.edu>
2018-05-03 18:42:51 -0400

    Added UI navigation, Respawn System, broken automated walk

---
+++ Don't Fall/Assets/Scripts/Respawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    //creates entry fields inside inspector
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {   //resets players position to respawn point
+        player.transform.position = respawnpoint.transform.position;
+    }
+
+}
